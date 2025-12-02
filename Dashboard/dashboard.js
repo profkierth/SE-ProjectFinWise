8054d3654f55f97dce9c54a7a2cf5db747a092a0
profkierth <munozkierthneil@gmail.com>
2025-12-02 22:56:48 +0800

    Update dashboard.js

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -1,1 +1,12 @@
+document.querySelectorAll(".menu-item").forEach(item => {
+    item.addEventListener("click", () => {
+        alert("This page will be added soon!");
+    });
+});
+
+document.querySelectorAll(".nav-item").forEach(item => {
+    item.addEventListener("click", () => {
+        alert("Navigation will be added soon!");
+    });
+});
 
