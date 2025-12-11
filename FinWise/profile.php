dc06627b3faa0dcf829fd89bf3d9075d734d28de
profkierth <munozkierthneil@gmail.com>
2025-12-11 21:20:36 +0800

    Update profile.php

--- FinWise/profile.php
+++ FinWise/profile.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
-if(!isset($_SESSION['user'])){ header("Location:index.php"); exit; }
+if(!isset($_SESSION['user'])){ 
+    header("Location:index.php"); 
+    exit; 
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,26 +13,33 @@ if(!isset($_SESSION['user'])){ header("Location:index.php"); exit; }
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body class="gradient">
+
 <div class="profile-header">
     <a href="dashboard.php" class="back-btn"><i class="fa-solid fa-arrow-left"></i></a>
+
     <div class="profile-top">
-        <img src="<?php echo htmlspecialchars($_SESSION['avatar'] ?? 'https://cdn-icons-png.flaticon.com/512/149/149071.png'); ?>" class="profile-avatar">
-        <h2 class="profile-name"><?php echo htmlspecialchars($_SESSION['fullname'] ?? $_SESSION['user']); ?></h2>
+        <img src="<?php echo htmlspecialchars($_SESSION['avatar'] ?? 'https://cdn-icons-png.flaticon.com/512/149/149071.png'); ?>" 
+             class="profile-avatar">
+
+        <h2 class="profile-name">
+            <?php echo htmlspecialchars($_SESSION['fullname'] ?? $_SESSION['user']); ?>
+        </h2>
     </div>
 </div>
 
 <div class="profile-menu">
+
     <a href="edit_profile.php" class="profile-item">
         <i class="fa-solid fa-user"></i>
         <span>Edit Profile</span>
     </a>
 
     <a href="security.php" class="profile-item">
-        <i class="fa-solid fa-shield-halved"></i>
+        <i class="fa-solid fa-shield"></i>
         <span>Security</span>
     </a>
 
-    <a href="settings.php" class="profile-item">
+    <a href="setting.php" class="profile-item">
         <i class="fa-solid fa-gear"></i>
         <span>Settings</span>
     </a>
@@ -38,6 +48,8 @@ if(!isset($_SESSION['user'])){ header("Location:index.php"); exit; }
         <i class="fa-solid fa-right-from-bracket"></i>
         <span>Logout</span>
     </a>
+
 </div>
+
 </body>
 </html>
