f5d400b2eba3366086172d4150bcd09722d3d8cb
profkierth <munozkierthneil@gmail.com>
2025-12-16 20:23:38 +0800

    Update finwise_app.sql

--- database/finwise_app.sql
+++ database/finwise_app.sql
@@ -8,37 +8,24 @@ CREATE TABLE transactions (
     amount DECIMAL(10,2) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE,
     FOREIGN KEY (category_id) REFERENCES categories(id)
+        ON DELETE CASCADE
 ) ENGINE=InnoDB;
-
-
-
 
 CREATE TABLE categories (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     user_id INT UNSIGNED NOT NULL,
-    name VARCHAR(50) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     type ENUM('income','expense') NOT NULL,
-    icon VARCHAR(50) DEFAULT 'fa-tags',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    icon VARCHAR(50) DEFAULT NULL,
+    color VARCHAR(20) DEFAULT NULL,
 
-    CONSTRAINT fk_categories_user
-        FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
-
-
-CREATE TABLE transactions1 (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    category_id INT,
-    type ENUM('income','expense') NOT NULL,
-    amount DECIMAL(10,2) NOT NULL,
-    month VARCHAR(20),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (category_id) REFERENCES categories(id)
-);
 
 CREATE TABLE notifications (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -53,21 +40,15 @@ CREATE TABLE notifications (
 ) ENGINE=InnoDB;
 
 
-INSERT INTO notifications (user_id, title, message, type)
-VALUES
-(1, 'New Expense Added', 'You recorded ₱250 for transportation.', 'expense'),
-(1, 'Income Update', 'Your ₱8,000 salary was added.', 'income'),
-(1, 'Weekly Report', 'Your weekly summary is now available.', 'report');
-
-INSERT INTO categories (name, type) VALUES
-('Salary','income'),
-('Food','expense'),
-('Transport','expense');
-
-INSERT INTO transactions (user_id, category_id, amount) VALUES
-(1, 4, 8000),
-(1, 2, 500),
-(1, 3, 250);
+/*
+INSERT INTO categories (user_id, name, type, icon, color) VALUES
+(1,'Salary','income','fa-coins','#2ecc71'),
+(1,'Business','income','fa-briefcase','#27ae60'),
+(1,'Food','expense','fa-burger','#e74c3c'),
+(1,'Transport','expense','fa-bus','#f39c12'),
+(1,'Bills','expense','fa-file-invoice','#c0392b'),
+(1,'Shopping','expense','fa-cart-shopping','#8e44ad');
+*/
 
 
 CREATE TABLE users (
@@ -87,4 +68,4 @@ CREATE TABLE users (
 select * from users;
 select * from transactions;
 select * from categories;
-select * from notification;
+select * from notifications;
